18dce88e73846af54015b0995ceba9abb41b7646
wberaldo92 <wellington.beraldo92@gmail.com>
2024-03-24 20:23:24 -0300

    tipos de dados, declaração de var, var e constantes

--- trilha-java-basico/java-tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ trilha-java-basico/java-tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+
+        String meuNome = "Wellington Beraldo";
+
+        double salarioMinimo = 2500.33;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
